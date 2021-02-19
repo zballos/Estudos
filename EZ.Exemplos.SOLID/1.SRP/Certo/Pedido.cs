511a9fc6c278a9582296964fb8404fabfbeaedb1
Edson Luiz Zeballos dos Santos Filho <edson.filho@ms.senac.br>
2021-02-18 21:55:17 -0400

    Complemento

--- EZ.Exemplos.SOLID/1.SRP/Certo/Pedido.cs
+++ EZ.Exemplos.SOLID/1.SRP/Certo/Pedido.cs
@@ -1,12 +1,15 @@
-﻿namespace _1.SRP.Certo
+﻿using _1.SRP._Base;
+using System;
+using System.Collections.Generic;
+
+namespace _1.SRP.Certo
 {
     public class Pedido
     {
-        public void CalcularTotal() { }
-        public void ObterProdutos() { }
-        public void ObterTotalDeProdutos() { }
-        public void ObterProduto() { }
-        public void AdicionarProduto() { }
-        public void DeletarProduto() { }
+        public void CalcularTotal(List<Produto> produtos) { /***/ }
+        public void ObterProdutos() { /***/ }
+        public void ObterTotalDeProdutos(List<Produto> produtos) { /***/ }
+        public void AdicionarProduto(Produto produto) { /***/ }
+        public void DeletarProduto(Produto produto) { /***/ }
     }
 }
--- EZ.Exemplos.SOLID/1.SRP/Errado/Pedido.cs
+++ EZ.Exemplos.SOLID/1.SRP/Errado/Pedido.cs
@@ -1,22 +1,25 @@
-﻿namespace _1.SRP.Errado
+﻿using _1.SRP._Base;
+using System;
+using System.Collections.Generic;
+
+namespace _1.SRP.Errado
 {
     public class Pedido
     {
-        public void CalcularTotal() { }
-        public void ObterProdutos() { }
-        public void ObterTotalDeProdutos() { }
-        public void ObterProduto() { }
-        public void AdicionarProduto() { }
-        public void DeletarProduto() { }
+        public void CalcularTotal(List<Produto> produtos) { /***/ }
+        public void ObterProdutos() { /***/ }
+        public void ObterTotalDeProdutos(List<Produto> produtos) { /***/ }
+        public void AdicionarProduto(Produto produto) { /***/ }
+        public void DeletarProduto(Produto produto) { /***/ }
 
 
-        public void ImprimirPedido() { }
-        public void MostrarPedido() { }
+        public void ImprimirPedido(Guid id) { /***/ }
+        public void MostrarPedido(Guid id) { /***/ }
 
 
-        public void CarregarPedido() { }
-        public void SalvarPedido() { }
-        public void AtualizarPedido() { }
-        public void DeletarPedido() { }
+        public void CarregarPedido(Guid id) { /***/ }
+        public void SalvarPedido(Pedido pedido) { /***/ }
+        public void AtualizarPedido(Pedido pedido) { /***/ }
+        public void DeletarPedido(Pedido pedido) { /***/ }
     }
 }
